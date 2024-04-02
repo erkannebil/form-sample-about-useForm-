c0a8e6fafc2fed24e9cf49b6dd1f8b09daa332b1
Nebi Resul Erkan <80630016+erkannebil@users.noreply.github.com>
2024-04-03 01:10:24 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-import GokalpForm from './Forms/GokalpForm'
-import GultekinForm from './Forms/gultekinForm'
-import SinanForm from './Forms/SinanForm'
-import EgemenForm from './Forms/EgemenForm'
-import HocaForm from './Forms/Hoca'
-import ElifForm from './Forms/ElifForm'
+
 import NebiForm from './Forms/NebiForms'
 
 
@@ -12,12 +7,6 @@ function App() {
 
   return (
     <>
-     <HocaForm />
-     <ElifForm />
-     <GokalpForm />
-     <GultekinForm />
-     <SinanForm />
-     <EgemenForm />
      <NebiForm />
     </>
   )
